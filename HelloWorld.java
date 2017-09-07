184b6e68292344f3851527bf7107264219cd75c5
Emma Beck Clausen <eclausen@cse.unl.edu>
2017-09-06 21:07:37 -0500

    commit message

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+//file:HelloWorldAssignment01
+//Author:Emma Clausen
+//date:9/6/2017
+//
+//This program tells the user hello
+public class HelloWorld {
+	   public static void main(String args[]) {
+		   System.out.println("Hello Emma");
+	   }
+}
